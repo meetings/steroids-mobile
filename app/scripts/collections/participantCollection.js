ac37fb26149b82426190070631b9500aed031999
Jussi Kaijalainen <fiddler@iki.fi>
2012-12-13 15:23:54 +0200

    Add comparator to order participants by RSVP

--- app/scripts/collections/participantCollection.js
+++ app/scripts/collections/participantCollection.js
@@ -8,6 +8,13 @@ app.participantCollection = Backbone.Collection.extend({
     },
 
     fetchMore: function(){
+    },
+
+    comparator : function( m ){
+        if( m.get('is_creator') ) return 1;
+        else if( m.get('rsvp_status' ) === 'yes' ) return 2;
+        else if( m.get('rsvp_status' ) === ' no' ) return 4;
+        else return 3;
     }
 
 });
