f7a4c483a79d662bf312a2c7dcf7b30f7e32b6a6
Jussi Kaijalainen <fiddler@iki.fi>
2013-03-05 08:58:56 +0200

    Added panel view link handling

--- app/scripts/views/panelView.js
+++ app/scripts/views/panelView.js
@@ -1,10 +1,37 @@
 app.panelView = Backbone.View.extend({
 
-    initialize: function(options) {
+    initialize : function(options) {
         this.menu_active = options.active;
     },
 
-    render: function() {
+    render : function() {
         this.$el.append( templatizer.panel( { active : this.menu_active } ) );
+    },
+
+    events : {
+        'click #nav-meetings' : 'openMeetings',
+        'click #nav-facebook' : 'redirectFacebook',
+        'click #nav-twitter' : 'redirectTwitter',
+        'click #nav-tos' : 'openTos',
+        'click #nav-logout' : 'logout'
+    },
+
+    openMeetings : function(e){
+        e.preventDefault();
+        window.location = '/meetings.html';
+    },
+    redirectFacebook : function(e){
+        e.preventDefault();
+    },
+    redirectTwitter : function(e){
+        e.preventDefault();
+    },
+    openTos : function(e){
+        e.preventDefault();
+    },
+    logout : function(e){
+        e.preventDefault();
+        app._removeAuthCookie();
+        window.location = '/login.html';
     }
 });
