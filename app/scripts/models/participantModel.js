be9fcfacc14a067d2f636c8294b8c56b5b28b757
Jussi Kaijalainen <fiddler@iki.fi>
2013-04-12 10:55:29 +0300

    Fixed last nights stupidities in userModel & participantModel

--- app/scripts/models/participantModel.js
+++ app/scripts/models/participantModel.js
@@ -1,7 +1,9 @@
 app.participantModel = Backbone.Model.extend({
     initialize : function( data, options ){
-        this.url = app.defaults.api_host + '/v1/meeting_participants/' + ( data.id || '' );
-    	this.on("change", function() {
+        if( data ){
+            this.url = app.defaults.api_host + '/v1/meeting_participants/' + ( data.id || '' );
+        }
+        this.on("change", function() {
             this.url = app.defaults.api_host + '/v1/meeting_participants/' + ( this.id || '' );
         }, this );
     },
--- app/scripts/models/userModel.js
+++ app/scripts/models/userModel.js
@@ -1,7 +1,9 @@
 app.userModel = Backbone.Model.extend({
     initialize : function( data, options ){
-        this.url = app.defaults.api_host + '/v1/users/' + ( data.id || 'me' );
-    	this.on("change", function() {
+        if( data ){
+            this.url = app.defaults.api_host + '/v1/users/' + ( data.id || 'me' );
+        }
+        this.on("change", function() {
             this.url = app.defaults.api_host + '/v1/users/' + ( this.id || 'me' );
         }, this );
     },
